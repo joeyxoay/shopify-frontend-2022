8ead2f014cc59abef43b2699397808ba758df46b
joeyxoay <58315852+joeyxoay@users.noreply.github.com>
2022-01-17 19:16:36 -0700

    Update SearchBar.js

--- nasa-api/src/components/SearchBar.js
+++ nasa-api/src/components/SearchBar.js
@@ -3,19 +3,25 @@ import TextField from '@material-ui/core/TextField';
 
 export function SearchBar({redirect}) {
   return (
-      <TextField 
-          id="standard-basic" 
-          variant="standard" 
-          type="search"
-          fullWidth
-          autoComplete='off'
-          style={{background: "white", borderRadius: 50, paddingLeft:10, paddingRight:10}}
-          onKeyPress={(e) => {
-              if (e.key === 'Enter') {
-                  redirect(e.target.value);
-              }
-          }}
-          placeholder={"Type here..."}
-      />
+      <form
+        onSubmit={e => {
+            e.preventDefault();
+        }}
+      >
+        <TextField 
+            id="standard-basic" 
+            variant="standard" 
+            type="search"
+            fullWidth
+            autoComplete='off'
+            style={{background: "white", borderRadius: 50, paddingLeft:10, paddingRight:10}}
+            onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                    redirect(e.target.value);
+                }
+            }}
+            placeholder={"Type here..."}
+        />
+      </form>
   );
 }
